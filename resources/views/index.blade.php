82427d807a1d542127f42f5ffd26fb54817beba2
Ian Felix <ianernesto031@gmail.com>
2026-02-10 14:58:02 -0700

    Actualizacion de imagenes y css, asi como archivos scss

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,15 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bienes Raices</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="{{ asset('js/bundle.min.js') }}" defer></script>
-
+    
 </head>
 <body>
-    <header class="header">
+    <header class="header inicio">
         <div class="contenedor contenido-header">
             <div class="barra">
                 <a href="/">
-                    <img src="/img/logo.svg" alt="Logotipo de Bienes Raices">
+                    <img src="{{ asset('img/logo.svg') }}" alt="Logo">
                 </a>
                 <nav class="navegacion">
                     <a href="{{ route('nosotros') }}">Nosotros</a>
@@ -27,9 +26,90 @@
         </div>
     </header>
     
-    <main class="contenerdor seccion">
-        <h1>Titulo Página</h1>
+    <main class="contenedor seccion">
+        <h1>Más Sobre Nosotros</h1>
+
+        <div class="iconos-nosotros">
+            <!--ICONO 1-->
+            <div class="icono">
+                <img src="{{ asset('img/icono1.svg') }}" alt="Icono seguridad" loading="lazy">
+                <h3>Seguridad</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Tenetur beatae corporis repellendus? Nobis doloribus soluta est quasi, 
+                    incidunt magni eveniet aliquid, totam, optio illum adipisci. 
+                    Assumenda laboriosam cupiditate eum libero?
+                </p>
+            </div>
+
+            <!--ICONO 2-->
+            <div class="icono">
+                <img src="{{ asset('img/icono2.svg') }}" alt="Icono Precio" loading="lazy">
+                <h3>Precio</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Tenetur beatae corporis repellendus? Nobis doloribus soluta est quasi, 
+                    incidunt magni eveniet aliquid, totam, optio illum adipisci. 
+                    Assumenda laboriosam cupiditate eum libero?
+                </p>
+            </div>
+            
+            <!--ICONO 3-->
+            <div class="icono">
+                <img src="{{ asset('img/icono3.svg') }}" alt="Icono Tiempo" loading="lazy">
+                <h3>A Tiempo</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Tenetur beatae corporis repellendus? Nobis doloribus soluta est quasi, 
+                    incidunt magni eveniet aliquid, totam, optio illum adipisci. 
+                    Assumenda laboriosam cupiditate eum libero?
+                </p>
+            </div>
+        </div>
     </main>
+
+    <!--Seccion de Casas y Departamentos en Venta-->
+    <section class="seccion">
+        <div class="contenedor"> <!--Todo esto esta dentro de la clase contenedor-->
+            <h2>Casas y Departamentos en Venta</h2>
+            <div class="contenedor-anuncios">
+                <div class="anuncio">
+                <picture>
+                    <source srcset="{{ asset('img/anuncio1.webp') }}" type="image/webp">
+                    <source srcset="{{ asset('img/anuncio1.jpg') }}" type="image/jpeg">
+                    <img loading="lazy" src="{{ asset('img/anuncio1.jpg') }}" alt="anuncio">
+                </picture>
+
+                <div class="contenido-anuncio"> <!--Anuncio 1-->
+                    <h3>Casa de Lujo en el lago</h3>
+                    <p>Casa en el lago con excelente vista, acabados de lujo a un excelente</p>
+                    <p class="precio">$3,0000,000</p>
+
+                    <ul class="iconos-caracteristicas">
+                        
+                        <li>
+                            <img loading="lazy" src="img/icono_wc.svg" alt="icono wc">
+                            <p>3</p>
+                        </li>
+                        
+                        <li>
+                            <img loading="lazy" src="img/icono_estacionamiento.svg" alt="icono wc">
+                            <p>3</p>
+                        </li>
+
+                        <li>
+                            <img loading="lazy" src="img/icono_dormitorio.svg" alt="icono wc">
+                            <p>3</p>
+                        </li>
+                    </ul>
+
+                    <a href="{{ route('anuncios') }}" class="boton boton-amarillo">
+                        Ver propiedad
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
 
     <footer class="footer seccion">
         <div class="contenedor contenerdor-footer">
@@ -43,7 +123,7 @@
         <p class="copyright">Todos los derechos reservados 2021 &copy;</p>
     </footer>
     
-    <script src="{{ asset('js/bundle.min.js') }}" defer></script>
+    <!--<script src="{{ asset('js/bundle.min.js') }}" defer></script>-->
 
 </body>
 </html>
